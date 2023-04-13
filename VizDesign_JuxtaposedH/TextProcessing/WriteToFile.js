721dc41e1b856e190b5cd1a72569e5b9d8af47ec
rachael27 <rachaelfernandez27@gmail.com>
2023-04-13 13:51:57 +0530

    Version with levellines, and solid,dashed borders around nodes

---
+++ VizDesign_JuxtaposedH/TextProcessing/WriteToFile.js
@@ -0,0 +1,27 @@
+
+/*
+const fs = require('fs');
+fs.writeFile('Output.txt', hdata, (err) => {
+
+    // In case of a error throw err.
+    if (err) throw err;
+});
+*/
+
+
+/* 
+
+// Import http and fs 
+const fs = require('fs')
+const http = require('http')
+
+// Creating Server 
+http.createServer((req, res) => {
+    console.log(res);
+    fs.writeFile('Output.txt', "hii", (err) => {
+
+        // In case of a error throw err.
+        if (err) throw err;
+    });
+
+}).listen(8080, 'localhost'); */
